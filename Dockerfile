c550a43805b8507170e62cf2cbfa06f8ae6a476c
Verchenko <via_atm@mail.ru>
2025-04-01 14:27:43 +0300

    create docker compose file end hide critical variables

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,3 @@ ENV PYTHONUNBUFFERED 1
 COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 COPY . /code
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80", "--reload"]
--- database.py
+++ database.py
@@ -1,9 +1,20 @@
 from contextlib import contextmanager
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session, Session
 
-DATABASE_URL = "postgresql+psycopg2://ilusha:12345@localhost/db"
+import os
+DB_HOST = os.getenv("DB_HOST")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_PORT = int(os.getenv("POSTGRES_PORT"))
+
+DATABASE_URL = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(POSTGRES_USER,
+                                                             POSTGRES_PASSWORD,
+                                                             DB_HOST,
+                                                             POSTGRES_PORT,
+                                                             POSTGRES_DB)
 
 engine = create_engine(
     DATABASE_URL,
